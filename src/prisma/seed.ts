d4a3d4ba5ddecc44fd1d4170a1bc6e03b82085f0
Gabriel Pires <gabrielpires2991@gmail.com>
2024-11-08 02:44:35 -0300

    finish model

--- src/prisma/seed.ts
+++ src/prisma/seed.ts
@@ -9,6 +9,7 @@ async function main() {
         title: 'Bateria',
         description:
           'As baterias levam de 500 a 1.000 anos para se degradar...',
+        img: 'https://firebasestorage.googleapis.com/v0/b/upload-images-16007.appspot.com/o/images_hackathon%2Fbateria.jpg?alt=media&token=7c60800a-e2f4-4a0f-bd9d-685ba03fb429',
         points: 80,
         weight: 250,
       },
@@ -16,17 +17,20 @@ async function main() {
         title: 'Celular',
         description: 'Um celular leva cerca de 1.000 anos para se decompor...',
         points: 70,
+        img: 'https://firebasestorage.googleapis.com/v0/b/upload-images-16007.appspot.com/o/images_hackathon%2Fcelulat.jpg?alt=media&token=21295e62-e6f5-485d-adf8-26399ac1d094',
         weight: 200,
       },
       {
         title: 'Impressora',
         description: 'As impressoras possuem cartuchos com tinta tóxica...',
+        img: 'https://firebasestorage.googleapis.com/v0/b/upload-images-16007.appspot.com/o/images_hackathon%2Fimpressora.jpg?alt=media&token=4d474f4c-734c-4633-a8a6-fe2b4914e0c3',
         points: 60,
         weight: 800,
       },
       {
         title: 'Mouse',
         description: 'O mouse leva cerca de 450 anos para se degradar...',
+        img: 'https://firebasestorage.googleapis.com/v0/b/upload-images-16007.appspot.com/o/images_hackathon%2Fmouse.jpg?alt=media&token=ddd5ea24-639c-41db-af35-ca9736a5ee81',
         points: 40,
         weight: 150,
       },
@@ -34,12 +38,14 @@ async function main() {
         title: 'Notebook',
         description:
           'Notebooks contêm metais e plásticos que demoram séculos para se decompor...',
+        img: 'https://firebasestorage.googleapis.com/v0/b/upload-images-16007.appspot.com/o/images_hackathon%2Fnotebook.jfif?alt=media&token=0f9cb131-b691-4b88-8dba-b6d2a40d1264',
         points: 90,
         weight: 1500,
       },
       {
         title: 'Placa Mãe',
         description: 'A placa mãe leva até 1.000 anos para se decompor...',
+        img: 'https://firebasestorage.googleapis.com/v0/b/upload-images-16007.appspot.com/o/images_hackathon%2Fplaca_mae.jpg?alt=media&token=f578c0b2-129b-459f-a1a9-58acb408e366',
         points: 50,
         weight: 500,
       },
@@ -47,6 +53,7 @@ async function main() {
         title: 'Teclado',
         description:
           'O teclado, feito de plástico, leva cerca de 500 anos para se decompor...',
+        img: 'https://firebasestorage.googleapis.com/v0/b/upload-images-16007.appspot.com/o/images_hackathon%2Fteclado.jpg?alt=media&token=eed72614-8687-41c6-9aef-3904859cd637',
         points: 40,
         weight: 200,
       },
@@ -54,6 +61,7 @@ async function main() {
         title: 'Televisão',
         description:
           'Uma televisão pode levar até 1.000 anos para se decompor...',
+        img: 'https://firebasestorage.googleapis.com/v0/b/upload-images-16007.appspot.com/o/images_hackathon%2Ftelevisao.jpg?alt=media&token=6c86f393-8029-4eff-b2d5-39a416b28676',
         points: 100,
         weight: 10000,
       },
